06d4b6ebc7471c95d4d954377afe8bcc08ce0502
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-15 14:45:42 +0500

    spec

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -3,10 +3,10 @@ $(document).ready(function () {
     $('a[data-click="modal"]').click(function () {
         var itemWindow = $(this).data('item');
         $(itemWindow).toggleClass('active');
-        $('.mask').toggleClass('active');
+        $('.mask, body').toggleClass('active');
     });
 
     $('.mask, .modal__close').click(function () {
-        $('.mask, .modal__close, .modal').removeClass('active');
+        $('.mask, .modal__close, .modal, body').removeClass('active');
     });
 });
