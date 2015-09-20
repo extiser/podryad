244082269a060ff8adf86550e36e8590df40a0d7
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-21 01:09:31 +0500

    contractor page

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -1,3 +1,21 @@
+$(document).ready(function () {
+    $('.contractor-block-info-show').click(function () {
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active').parent().children('span').empty();
+            $(this).html($(this).text().replace('скрыть', 'показать'));
+        } else {
+            var dataShow = $(this).data('show');
+            $(this).addClass('active').parent().append('<span>' + dataShow + '</span>');
+            $(this).html($(this).text().replace('показать', 'скрыть'));
+        }
+    });
+    $('.contractor-tabs-control__item').click(function () {
+        var indexTab = $(this).index();
+        $(this).addClass('active').siblings().removeClass('active');
+        $('.contractor-tabs-box__item').eq(indexTab).addClass('active').siblings().removeClass('active');
+        return false;
+    });
+});
 
 $(document).ready(function () {
     $('a[data-click="modal"]').click(function () {
