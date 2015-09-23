a182eae57c4558bd287093f01eb5fd98d1cbff76
Руслан Усманов <extiser@MacBook-Pro-Ruslan.local>
2015-09-23 11:55:55 +0500

    add styles for drop menu & add change city

--- builds/static/js/main.js
+++ builds/static/js/main.js
@@ -68,7 +68,22 @@ $(document).ready(function () {
         useSVG: false
     });
 })(jQuery);
-
+$(document).ready(function () {
+    var currentValue = $('.header-city-select__current').html();
+    $('.header-city-select__input').val(currentValue);
+    $('.header-city-select').click(function () {
+        $(this).children('.header-city-select__list').slideToggle();
+    });
+    $('.header-city-select__option').click(function () {
+        var optionValue = $(this).html();
+        var thisValue = $('.header-city-select__current').html();
+        $('.header-city-select__input').val(optionValue);
+        $('.header-city-select__current').html(optionValue);
+        // destroy
+        $(this).remove();
+        $('.header-city-select__list').append('<div class="header-city-select__option">' + thisValue + '</div>');
+    });
+});
 $(document).ready(function () {
     $('a[data-click="modal"]').click(function () {
         var itemWindow = $(this).data('item');
